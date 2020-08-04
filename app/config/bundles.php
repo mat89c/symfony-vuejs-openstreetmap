8f1bbddfdea0b0c1240d0778b49278237f70d6a8
Mateusz Czajka <mat89c@mat89c>
2020-08-04 19:49:03 +0200

    Add DoctrineFixturesBundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
