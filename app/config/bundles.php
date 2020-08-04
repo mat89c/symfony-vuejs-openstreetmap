6c6a244166508ba762890ef48ff83267258b27dc
Mateusz Czajka <mat89c@mat89c>
2020-08-04 19:18:07 +0200

    Add symfony security

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
