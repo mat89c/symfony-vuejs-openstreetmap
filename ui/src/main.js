6dfa80af7969647fec90b4347b8ac80f2ef1d0c6
Mateusz Czajka <mat89c@mat89c>
2020-08-05 11:03:06 +0200

    Add vuex and vuecookies

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
+import VueCookies from 'vue-cookies';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,6 +10,8 @@ import validation from './plugins/validation';
 Vue.config.productionTip = false;
 
 Vue.use(validation);
+Vue.use(Vuex);
+Vue.use(VueCookies);
 
 new Vue({
   router,
