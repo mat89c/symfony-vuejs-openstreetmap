f6fb89e0cab6b5b87fdebd0273e5c4d45e9bf5fd
Mateusz Czajka <mat89c@mat89c>
2020-08-17 11:33:27 +0200

    Change vuetify language

--- ui/src/plugins/vuetify.js
+++ ui/src/plugins/vuetify.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import pl from 'vuetify/src/locale/pl.ts';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { pl },
+    current: 'pl',
+  },
   theme: {
     dark: true,
   },
